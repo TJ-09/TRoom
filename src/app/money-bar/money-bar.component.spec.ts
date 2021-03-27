fea93ca6bfe7aec80e11d51fd7ff0a4d4ffad5ce
TJ-09 <tim.forman90@gmail.com>
2021-03-27 08:56:17 +0000

    angular bump to 11

--- src/app/money-bar/money-bar.component.spec.ts
+++ src/app/money-bar/money-bar.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MoneyBarComponent } from './money-bar.component';
 
@@ -6,7 +6,7 @@ describe('MoneyBarComponent', () => {
   let component: MoneyBarComponent;
   let fixture: ComponentFixture<MoneyBarComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MoneyBarComponent ]
     })
