a9070802dd7a913593514fd1ab6e365621d4a966
TJ-09 <tim.forman90@gmail.com>
2021-04-02 19:50:04 +0100

    product updates

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,57 +1,98 @@
-
 // Edit the below as required for the products, buttons will create themselves
-// to find the icon text please use the link below, click the icon, 
+// to find the icon text please use the link below, click the icon,
 // the in the bottom right expand the 'selected icon' and it is the text next to the icon
 // https://material.io/resources/icons/?icon=adb&style=outline
 
-
 export const HatchProducts = [
-    { itemName: 'Cake', cost: 1.8, icon: 'cake', amount: 1 },
-    { itemName: 'Cake Special', cost: 2.5, icon: 'cake', amount: 1 },
-    { itemName: 'Cream Tea', cost: 2.5, icon: 'scatter_plot', amount: 1 },
-    { itemName: 'Scone', cost: 1.3, icon: 'fiber_manual_record', amount: 1 },
-    { itemName: 'Filter Coffee', cost: 1.2, icon: 'local_cafe', amount: 1 },
-    { itemName: 'Decaf Coffee', cost: 0.7, icon: 'local_cafe', amount: 1 },
-    { itemName: 'Juice', cost: 0.3, icon: 'local_drink', amount: 1 },
-    { itemName: 'Cookbook', cost: 7.5, icon: 'menu_book', amount: 1 },
-
-]
+  { itemName: "Cake", cost: 1.8, icon: "cake", amount: 1 },
+  { itemName: "Cake Special", cost: 2.5, icon: "cake", amount: 1 },
+  { itemName: "Cream Tea", cost: 2.5, icon: "scatter_plot", amount: 1 },
+  { itemName: "Scone", cost: 1.3, icon: "fiber_manual_record", amount: 1 },
+  { itemName: "Filter Coffee", cost: 1.2, icon: "local_cafe", amount: 1 },
+  { itemName: "Decaf Coffee", cost: 0.7, icon: "local_cafe", amount: 1 },
+  { itemName: "Juice", cost: 0.3, icon: "local_drink", amount: 1 },
+  { itemName: "Cookbook", cost: 7.5, icon: "menu_book", amount: 1 },
+];
 
 export const IndoorProducts = [
-    { itemName: 'Cake', cost: 1.8, icon: 'cake', amount: 1 },
-    { itemName: 'Cake Special', cost: 2.5, icon: 'cake', amount: 1 },
-    { itemName: 'Cream Tea', cost: 2.5, icon: 'scatter_plot', amount: 1 },
-    { itemName: 'Scone', cost: 1.3, icon: 'fiber_manual_record', amount: 1 },
-    { itemName: 'Filter Coffee', cost: 1.2, icon: 'local_cafe', amount: 1 },
-    { itemName: 'Decaf Coffee', cost: 0.7, icon: 'local_cafe', amount: 1 },
-    { itemName: 'Juice', cost: 0.3, icon: 'local_drink', amount: 1 },
-    { itemName: 'Cookbook', cost: 7.5, icon: 'menu_book', amount: 1 },
-
-]
+  { itemName: "Cake", cost: 2.0, icon: "cake", amount: 1 },
+  { itemName: "Cake Special", cost: 3.0, icon: "cake", amount: 1 },
+  { itemName: "Cream Tea", cost: 3.0, icon: "scatter_plot", amount: 1 },
+  { itemName: "Scone", cost: 1.5, icon: "fiber_manual_record", amount: 1 },
+  { itemName: "Filter Coffee", cost: 2.0, icon: "local_cafe", amount: 1 },
+  { itemName: "Decaf Coffee", cost: 1.0, icon: "local_cafe", amount: 1 },
+  { itemName: "Juice", cost: 0.3, icon: "local_drink", amount: 1 },
+  { itemName: "Cookbook", cost: 7.5, icon: "menu_book", amount: 1 },
+];
 
 export const HatchTea = [
-    { itemName: 'Tea (mug)', cost: 1.2, icon: 'emoji_food_beverage', amount: 1 },
-]
+  { itemName: "Tea (mug)", cost: 1, icon: "emoji_food_beverage", amount: 1 },
+];
 
+// ignore this it opens the menu and shows the indoor tea type
 export const IndoorTea = [
-    { itemName: 'Pot of Tea', cost: 0, icon: 'free_breakfast', amount: 1 },
-]
+  { itemName: "Pot of Tea", cost: 0, icon: "free_breakfast", amount: 1 },
+];
 
 export const HatchTeaType = [
-    { itemName: 'English Breakfast', cost: 1.2, icon: 'emoji_food_beverage', amount: 1 },
-    { itemName: 'Earl Grey', cost: 1.2, icon: 'emoji_food_beverage', amount: 1 },
-    { itemName: 'Decaf', cost: 1.2, icon: 'emoji_food_beverage', amount: 1 },
-    { itemName: 'Fruit Tea', cost: 1.2, icon: 'emoji_food_beverage', amount: 1 },
-]
+  {
+    itemName: "English Breakfast",
+    cost: 1,
+    icon: "emoji_food_beverage",
+    amount: 1,
+  },
+  { itemName: "Earl Grey", cost: 1, icon: "emoji_food_beverage", amount: 1 },
+  { itemName: "Decaf", cost: 1, icon: "emoji_food_beverage", amount: 1 },
+  { itemName: "Fruit Tea", cost: 1, icon: "emoji_food_beverage", amount: 1 },
+];
 
 export const IndoorTeaType = [
-    { itemName: 'Pot 1 English Breakfast', cost: 1.2, icon: 'emoji_food_beverage', amount: 1 },
-    { itemName: 'Pot 1 Earl Grey', cost: 1.2, icon: 'emoji_food_beverage', amount: 1 },
-    { itemName: 'Pot 1 Decaf', cost: 1.2, icon: 'emoji_food_beverage', amount: 1 },
-    { itemName: 'Pot 1 Fruit Tea', cost: 1.2, icon: 'emoji_food_beverage', amount: 1 },
-    { itemName: 'Pot 2 English Breakfast', cost: 2.4, icon: 'emoji_food_beverage', amount: 1 },
-    { itemName: 'Pot 2 Earl Grey', cost: 2.4, icon: 'emoji_food_beverage', amount: 1 },
-    { itemName: 'Pot 2 Decaf', cost: 2.4, icon: 'emoji_food_beverage', amount: 1 },
-    { itemName: 'Pot 2 Fruit Tea', cost: 2.4, icon: 'emoji_food_beverage', amount: 1 },
-
-]
+  {
+    itemName: "Pot 1 English Breakfast",
+    cost: 1.2,
+    icon: "emoji_food_beverage",
+    amount: 1,
+  },
+  {
+    itemName: "Pot 1 Earl Grey",
+    cost: 1.2,
+    icon: "emoji_food_beverage",
+    amount: 1,
+  },
+  {
+    itemName: "Pot 1 Decaf",
+    cost: 1.2,
+    icon: "emoji_food_beverage",
+    amount: 1,
+  },
+  {
+    itemName: "Pot 1 Fruit Tea",
+    cost: 1.2,
+    icon: "emoji_food_beverage",
+    amount: 1,
+  },
+  {
+    itemName: "Pot 2 English Breakfast",
+    cost: 2.4,
+    icon: "emoji_food_beverage",
+    amount: 1,
+  },
+  {
+    itemName: "Pot 2 Earl Grey",
+    cost: 2.4,
+    icon: "emoji_food_beverage",
+    amount: 1,
+  },
+  {
+    itemName: "Pot 2 Decaf",
+    cost: 2.4,
+    icon: "emoji_food_beverage",
+    amount: 1,
+  },
+  {
+    itemName: "Pot 2 Fruit Tea",
+    cost: 2.4,
+    icon: "emoji_food_beverage",
+    amount: 1,
+  },
+];
